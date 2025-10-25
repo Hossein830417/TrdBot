fcdff6692d746812bc9934b95502928e09f3be11
Hossein830417 <82313632+Hossein830417@users.noreply.github.com>
2025-10-25 11:48:58 +0330

    Update bb_strategy.py

--- strategies/bb_strategy.py
+++ strategies/bb_strategy.py
@@ -1,1 +1,17 @@
+import pandas as pd
+from ta.volatility import BollingerBands
 
+def should_trade(exchange, symbol):
+    ohlcv = exchange.fetch_ohlcv(symbol, '1m', limit=20)
+    df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    bb = BollingerBands(df['close'], window=20, window_dev=2)
+
+    current_close = df['close'].iloc[-1]
+    bb_high = bb.bollinger_hband().iloc[-1]
+    bb_low = bb.bollinger_lband().iloc[-1]
+
+    if current_close < bb_low:
+        return 'buy'
+    elif current_close > bb_high:
+        return 'sell'
+    return None
