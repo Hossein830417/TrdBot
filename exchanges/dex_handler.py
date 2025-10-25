fc4a88cd1919c084f53e3c71baa8bc242fd0dd30
Hossein830417 <82313632+Hossein830417@users.noreply.github.com>
2025-10-25 11:49:48 +0330

    Update dex_handler.py

--- exchanges/dex_handler.py
+++ exchanges/dex_handler.py
@@ -1,1 +1,10 @@
+from web3 import Web3
 
+class DEXHandler:
+    def __init__(self, rpc_url, private_key):
+        self.web3 = Web3(Web3.HTTPProvider(rpc_url))
+        self.account = self.web3.eth.account.from_key(private_key)
+
+    def swap_tokens(self, token_in, token_out, amount):
+        # Placeholder for Uniswap swap logic
+        pass
