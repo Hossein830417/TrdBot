12bcbcc29d7e73aa23de181714fc470b9353b465
Hossein830417 <82313632+Hossein830417@users.noreply.github.com>
2025-10-25 11:49:36 +0330

    Update cex_handler.py

--- exchanges/cex_handler.py
+++ exchanges/cex_handler.py
@@ -1,1 +1,15 @@
+import ccxt
 
+class CEXHandler:
+    def __init__(self, exchange_name, api_key, secret):
+        self.exchange = getattr(ccxt, exchange_name)({
+            'apiKey': api_key,
+            'secret': secret,
+            'enableRateLimit': True
+        })
+
+    def fetch_price(self, symbol):
+        return self.exchange.fetch_ticker(symbol)['last']
+
+    def place_order(self, symbol, side, amount):
+        return self.exchange.create_market_order(symbol, side, amount)
